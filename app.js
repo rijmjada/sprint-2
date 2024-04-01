c6292be24f15e3b8a6d66cb987ec06721aeda50f
diego <rijmjada@gmail.com>
2024-04-01 15:10:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Importar el módulo express
+const express = require('express');
+
+// Crear una instancia de express
+const app = express();
+
+// Definir una ruta para el mensaje "Hola Mundo"
+app.get('/', (req, res) => {
+  res.send('Microservicio de Ventas');
+});
+
+// Iniciar el servidor en el puerto 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
