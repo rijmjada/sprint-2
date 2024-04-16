eefcb8fadd38580fcb7cdfaf51f1feecf39240b0
Diego Ormeño <72719844+rijmjada@users.noreply.github.com>
2024-04-15 23:18:57 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-echo "Hola mundo"
+#!/bin/bash
+
+# Descargar el archivo node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+
+# Descomprimir el archivo
+tar -xvf node_exporter-1.7.0.linux-amd64.tar.gz
+
+# Mover el binario a /usr/local/bin/
+sudo mv node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
+
+# Crear el usuario node_exporter
+sudo useradd -rs /bin/false node_exporter
+
+# Crear el archivo de servicio systemd
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOT
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+# Iniciar y habilitar el servicio
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+sudo systemctl daemon-reload
